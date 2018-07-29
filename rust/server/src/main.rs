e898aad897221fdc825c39508153370e386e8c25
meritozh <ah841814092@gmail.com>
2018-07-29 23:37:29 +0800

    [rust] complete server

--- rust/server/src/main.rs
+++ rust/server/src/main.rs
@@ -3,12 +3,19 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs::File;
 
+extern crate server;
+use server::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8090").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -16,27 +23,25 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    if validate_request(&buffer) {
-        let mut file = File::open("data/hello.html").unwrap();
-        let mut content = String::new();
-        file.read_to_string(&mut content).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-        // CRLF for network content
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", content);
-        stream.write(response.as_bytes()).unwrap();
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "data/hello.html")
     } else {
-        let response = "HTTP/1.1 200 OK\r\n\r\nFailed";
-        stream.write(response.as_bytes()).unwrap();
-    }
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "data/404.html")
+    };
 
+    let response = format!("{}{}", status_line, get_content(filename));
+
+    stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
 
-fn validate_request(buffer: &[u8; 512]) -> bool {
-    let get = b"GET / HTTP/1.1\r\n";
-    if buffer.starts_with(get) {
-        true
-    } else {
-        false
-    }
+#[inline]
+fn get_content(filename: &str) -> String {
+    let mut file = File::open(filename).unwrap();
+    let mut content = String::new();
+    file.read_to_string(&mut content).unwrap();
+
+    content
 }
